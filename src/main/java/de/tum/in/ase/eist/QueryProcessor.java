7ed9d822076da277b1927d6955fd326c763ccea0
appeon_a_lew <Yp8EdWwnO6j1ZlRR>
2022-07-13 16:59:51 +0200

    name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,9 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Ismail";
-        } else { // TODO extend the programm here
-            return "";
+        } else if(query.contains("plus")) {
+            return "2021";
         }
+        return "";
     }
 }
